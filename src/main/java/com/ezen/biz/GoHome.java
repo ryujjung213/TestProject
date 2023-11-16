a18629d386711eb0b0d231dcaa04bfe87f1b409a
HyoJung <ryujjung213@naver.com>
2023-11-16 15:29:38 +0900

    2호선을 탑시다.

---
+++ src/main/java/com/ezen/biz/GoHome.java
@@ -0,0 +1,28 @@
+package com.ezen.biz;
+
+public class GoHome {
+
+	private String go;
+	private String home;
+	private String now;
+	public String getGo() {
+		return go;
+	}
+	public void setGo(String go) {
+		this.go = go;
+	}
+	public String getHome() {
+		return home;
+	}
+	public void setHome(String home) {
+		this.home = home;
+	}
+	public String getNow() {
+		return now;
+	}
+	public void setNow(String now) {
+		this.now = now;
+	}
+	
+	
+}
