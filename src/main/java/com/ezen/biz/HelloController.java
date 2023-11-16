1fa8d250ae05be0925a9c22ca0ac3892d8ecdef6
HyoJung <ryujjung213@naver.com>
2023-11-16 14:23:21 +0900

    HelloController 완료!

---
+++ src/main/java/com/ezen/biz/HelloController.java
@@ -0,0 +1,12 @@
+package com.ezen.biz;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HelloController {
+	
+	public void hello() {
+		System.out.println("집에 가고 싶습니다~");
+	}
+
+}
